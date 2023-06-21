39de1407c7795ec2d171c6b35bab5bcb9181f1b9
Rahil Khan <rashidali.rahil@gmail.com>
2023-06-21 14:06:42 +0530

    Added hint for sequential thread execution

--- src/main/java/com/example/demo/javafeatures/concurrency/coding/SequentialExecutionCallable.java
+++ src/main/java/com/example/demo/javafeatures/concurrency/coding/SequentialExecutionCallable.java
@@ -1,5 +1,8 @@
 package com.example.demo.javafeatures.concurrency.coding;
 
+/**
+ * https://medium.com/@257ramanrb/print-a-sequence-using-3-threads-java-6f75a5072ae6
+ */
 public class SequentialExecutionCallable implements Runnable {
 
     @Override
